dab3e7b915171b65ab9f8c40250dbf57972725d9
Mohamed khalil <khm40005000@outlook.com>
2024-08-04 15:38:39 +0300

    some changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,27 @@ index.py file
 
 from api.v1.views import app_views
 from flask import jsonify
+import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """simple view status"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    obj = {}
+    "view stats of each object type"
+    for key, value in classes.items():
+        obj[key] = models.storage.count(value)
+    return jsonify(obj)

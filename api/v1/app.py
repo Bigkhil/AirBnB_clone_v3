85f610a13cf6ff727b0628f711a848cdd383ab04
Mohamed khalil <khm40005000@outlook.com>
2024-08-04 12:28:36 +0300

    some changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,6 +21,6 @@ def tear_down():
 if __name__ == "__main__":
     app.run(
             host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', 5000)),
+            port=int(getenv('HBNB_API_PORT', '5000')),
             threaded=True
             )
